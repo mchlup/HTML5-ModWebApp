6a125da8f9420e01154585545b20a0729a6f4876
mchlup <132652169+mchlup@users.noreply.github.com>
2025-12-10 07:42:08 +0100

    Delete modules directory

--- modules/dashboard/lang_cs.js
+++
@@ -1,9 +0,0 @@
-// Český překlad pro modul "dashboard".
-
-export default {
-  title: "Dashboard",
-  welcome: "Vítej, {username}",
-  modules: "Přehled modulů",
-  widgets: "Připraveno pro budoucí widgety",
-};
-
